4dabb04f15b8e718781e326e3f90416f9cbdd9c8
RafaPolit <rafaelpolit@gmail.com>
2020-10-06 18:15:52 -0500

    Fixed paths and added script

--- scripts/insert_raw_files.js
+++ scripts/insert_raw_files.js
@@ -2,10 +2,7 @@ import path from "path";
 import { CSVLoader } from "../csv/csvLoader.js";
 
 const insertFiles = async () => {
-  const csvFile = path.join(
-    path.resolve(path.dirname("")),
-    "/../MetObjects.csv"
-  );
+  const csvFile = path.join(path.resolve(path.dirname("")), "/MetObjects.csv");
   const loader = new CSVLoader();
   await loader.load(csvFile);
 };
